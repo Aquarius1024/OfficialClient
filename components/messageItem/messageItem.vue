33cf26693c4d179c0ea9252ec903654b843a0c24
Aquarius1024 <1125195001@qq.com>
2020-04-19 21:54:58 +0800

    更新项目文件样式，添加新的聊天组件MessageItem

---
+++ components/messageItem/messageItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <view class="message_block flex_aic">
+    <view class="message_icon"><image src="/static/img/avatar0.jpg" mode="aspectFill"></image></view>
+    <view class="flex_aic_jcsb message_content">
+      <view class="message_info">
+        <view>丁酉</view>
+        <view>菜鸟果果： 【反倒是佛教圣地佛反对舒服撒到佛山的杰佛森多久教四大覅哦】</view>
+      </view>
+      <view class="message_time">
+        <text v-if="notToday">9月20日</text>
+        <text v-else>20:30</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  computed: {
+    notToday() {
+      return true;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+$messageSize: 110upx;
+  
+.message_block {
+  margin: 10upx 20upx;
+  margin-right: 0;
+  
+  .message_icon {
+    height: $messageSize - 20;
+    width: $messageSize - 20;
+    flex-shrink: 0;
+    border-radius: 50%;
+    overflow: hidden;
+    background-color: $uni-color-primary;
+
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  
+  .message_content {
+    flex: 1;
+    margin-left: 20upx;
+    padding-right: 15upx;
+    height: $messageSize;
+    border-bottom: 1px solid $uni-text-color-disable;
+  }
+
+  .message_info {
+    view {
+      white-space: nowrap; /*强制内容在一行显示*/
+      overflow: hidden; /*超出部分溢出*/
+      text-overflow: ellipsis; /*溢出的部分使用省略号*/
+      max-width: 450upx;
+    }
+
+    view:first-of-type {
+      color: $uni-text-color;
+      font-size: 30upx;
+    }
+
+    view:last-of-type {
+      color: $uni-text-color-grey;
+    }
+  }
+
+  .message_time {
+    text-align: center;
+    color: $uni-text-color-grey;
+  }
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,19 +1,30 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+    <message-item></message-item>
+    <message-item></message-item>
+    <message-item></message-item>
+    <message-item></message-item>
+    <message-item></message-item>
+    <message-item></message-item>
+    <message-item></message-item>
+    
 	</view>
 </template>
 
 <script>
+  import messageItem from "../../components/messageItem/messageItem.vue";
+  
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
+    
+    components: {
+      messageItem,
+    },
+    
 		onLoad() {
 
 		},
@@ -24,29 +35,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
